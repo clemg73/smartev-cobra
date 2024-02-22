779fabd95441e795d421e048440612f321480cf2
clemg73 <65182326+clemg73@users.noreply.github.com>
2024-02-22 16:31:16 +0100

    station information first commit

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,9 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
+body {
+    font-family: 'Roboto', sans-serif;
+}
 </style>
