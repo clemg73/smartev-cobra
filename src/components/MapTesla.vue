e2a54120eb127da7bfcaf80849697890c65938f0
KilianVLT <kilian.vlt1@gmail.com>
2024-02-25 16:55:09 +0000

    Evolve

--- src/components/MapTesla.vue
+++ src/components/MapTesla.vue
@@ -1,13 +1,13 @@
 <template>
-  <l-map ref="map" :zoom="zoom" :center="center">
+  <l-map ref="map" id="map" :zoom="zoom" :center="center">
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-marker v-for="(marker, index) in markers" :key="index" :lat-lng="[marker.AddressInfo.Latitude, marker.AddressInfo.Longitude]" ></l-marker>
+    <l-marker v-for="(marker, index) in markers" @click="$emit('stationInfo', marker)" :key="index" :lat-lng="[marker.AddressInfo.Latitude, marker.AddressInfo.Longitude]" >
+    </l-marker>
   </l-map>
 </template>
 
 <script>
 import {LMap, LTileLayer, LMarker} from 'vue3-leaflet';
-import { Icon } from 'leaflet';
 
 const API_KEY = "7d563b7b-c646-4bfc-bd61-e260939a27bf";
 const zoom = 15;
@@ -35,20 +35,11 @@ async function getStations(){
 
 }
 
-console.log(await getStations());
-
-delete Icon.Default.prototype._getIconUrl;
-Icon.Default.mergeOptions({
-  iconRetinaUrl: null,
-  iconUrl: require('../assets/marker-2x.svg'),
-  shadowUrl: null,
-});
-
 export default {
   components: {
     LMap,
     LTileLayer,
-    LMarker
+    LMarker  
   },
   data () {
     return {
@@ -56,10 +47,9 @@ export default {
       attribution:
         '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       zoom: zoom,
-      iconUrl: "../assets/marker-2x.svg",
       center: [51.505, -0.159],
       markerPos: [51.505, -0.159],
-      markers: []
+      markers: [],
     };
   },
   async mounted(){
@@ -68,12 +58,16 @@ export default {
   methods: {
     async callGetStations(){
       try{
-        const stations = await getStations()
+        const stations = getStations()
         this.markers = stations
       }
       catch(err){
         console.error("Problème pour récupérer les données : " + err)
       }
+    },
+
+    checkBounds(bounds){
+      console.log(bounds);
     }
   }
 }
