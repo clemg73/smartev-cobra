0bfddb5acc3654a2f59702551c7159b28b7737ea
KilianVLT <kilian.vlt1@gmail.com>
2024-02-25 12:05:37 +0000

    Chargement de la map et des données d'une manière plus "VueJS"

--- src/components/MapTesla.vue
+++ src/components/MapTesla.vue
@@ -1,12 +1,16 @@
 <template>
-  <div id="map-tesla"></div>
+  <l-map ref="map" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-marker v-for="(marker, index) in markers" :key="index" :lat-lng="[marker.AddressInfo.Latitude, marker.AddressInfo.Longitude]" ></l-marker>
+  </l-map>
 </template>
 
 <script>
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import {LMap, LTileLayer, LMarker} from 'vue3-leaflet';
+import { Icon } from 'leaflet';
 
 const API_KEY = "7d563b7b-c646-4bfc-bd61-e260939a27bf";
+const zoom = 15;
 
 //function to get all charging stations of Tesla 
 async function getStations(){
@@ -31,45 +35,54 @@ async function getStations(){
 
 }
 
-let markerSVG = "../assets/marker.svg" 
+console.log(await getStations());
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: null,
+  iconUrl: require('../assets/marker-2x.svg'),
+  shadowUrl: null,
+});
 
 export default {
-  name: "map-tesla",
-  async mounted() {
-    L.Icon.Default.imagePath = '../assets/';
-    L.Icon.Default.prototype.options.iconUrl = markerSVG;
-    
-    // Créer une carte Leaflet et l'ajouter à l'élément avec l'id 'map'
-    this.map = L.map('map-tesla').setView([45.780835, 4.8720641], 6);
-
-    // Ajouter une couche de tuiles (par exemple OpenStreetMap) à la carte
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-      attribution: '© OpenStreetMap contributors'
-    }).addTo(this.map);
-
-    const stations = await getStations();
-
-    stations.forEach(station => {
-      let lat = station.AddressInfo.Latitude;
-      let lng = station.AddressInfo.Longitude;
-
-      // Ajouter un marqueur à la carte
-      let marker = L.marker([lat, lng]).addTo(this.map)
-      marker.addEventListener("click", ()=>{
-        // Récupère les infos d'un marqueur cliqué
-        let stationInfo = stations.filter((station) => marker._latlng.lat == station.AddressInfo.Latitude && marker._latlng.lng == station.AddressInfo.Longitude)[0]
-        this.$emit("stationInfo", stationInfo)
-        console.log(stationInfo);
-      })
-    });
-    
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  data () {
+    return {
+      url: 'https:{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: zoom,
+      iconUrl: "../assets/marker-2x.svg",
+      center: [51.505, -0.159],
+      markerPos: [51.505, -0.159],
+      markers: []
+    };
+  },
+  async mounted(){
+    await this.callGetStations();
+  },
+  methods: {
+    async callGetStations(){
+      try{
+        const stations = await getStations()
+        this.markers = stations
+      }
+      catch(err){
+        console.error("Problème pour récupérer les données : " + err)
+      }
+    }
   }
 }
+
 </script>
 
-<style>
-#map-tesla {
-  height: 100vh;
-  width: 100vw;
+<style scoped>
+#map {
+  height: 100vh!important;
+  width: 100vw!important;
 }
 </style>
